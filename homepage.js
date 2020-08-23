1c90c70b5bab5558df8fcb64cc11cbdfc06f5db8
Alba Mulero Otón <albamulero@MacBook-Pro-de-Alba.local>
2020-08-23 00:33:17 -0500

    agregando estilos

--- homepage.js
+++ homepage.js
@@ -22,8 +22,8 @@ function lienzo() {
 
     // Dibujamos las lineas
 
-    ctx.strokeStyle = "black";
-    ctx.stroke();
+    /*ctx.strokeStyle = "black";
+    ctx.stroke();*/
 
     // Creamos un bucle para generar los numeros y asignar su posicion en el tablero
 
@@ -34,13 +34,7 @@ function lienzo() {
 
     // Ponemos a la escucha la grilla
 
-     $canvas.addEventListener("click", function(e) {
-
-        let pos_x = 0
-        let pos_y = 0
-        let pos_xx = 50
-        let pos_yy = 50
-        let vueltas
+    $canvas.addEventListener("click", function(e) {
 
         // Este es un metodo para restar el margen del body y que solo me salga el valor de la grilla
 
@@ -57,9 +51,8 @@ function lienzo() {
 
             if (nclientX > numeros[n][2] && nclientX < numeros[n][4] && nclientY > numeros[n][1] && nclientY < numeros[n][3]){
                 
-                // Pasarmos el numero al validador del juego
+                // Pasamos el numero al validador del juego
                 regla_juego(numeros[n][0])
-
 
             }
         }
@@ -68,17 +61,12 @@ function lienzo() {
 
     function rellenar_cuadro() {
 
-        let valor_anadir_x = 50
-        let vueltas = 10
-        let valor_x = 10
-        let valor_y = 10
-        let posicion_array_numero = 0
-
         // Voy a recorrer las columnas
 
         for(n = 0; n < 100; n++){
-
+            ctx.font = "14px arial"
             ctx.fillText(numeros[n][0], numeros[n][2]+ 10, numeros[n][1] + 10)
+            
 
         }
     }
@@ -92,12 +80,11 @@ function lienzo() {
 
         for (n = 0; n <= 100; n++) {
 
-
             //console.log(n)
             let aleatorio = Math.round(Math.random() * 1000);
             //console.log(aleatorio);
             numeros.push([aleatorio, pos_x, pos_y, pos_xx, pos_yy])
-                //numeros[0].push(aleatorio)
+            //numeros[0].push(aleatorio)
 
             pos_x = pos_x + 50
             pos_xx = pos_xx + 50
@@ -109,7 +96,7 @@ function lienzo() {
                 pos_yy = pos_yy + 50
             }
                 
-            console.log(numeros[n][0] + " : " + numeros[n][1] + " : "  + numeros[n][2])
+           //console.log(numeros[n][0] + " : " + numeros[n][1] + " : "  + numeros[n][2])
 
         }
 
@@ -120,39 +107,37 @@ function lienzo() {
 
         numero_mas_bajo = true
 
-
         // Vamos a comporbar que es el numero mas bajo del array...
         // Recorremos el array y comprobamos que no encontremos ninguno mayor
+        
         for(n = 0; n < 100; n++){
 
             console.log(numeros[n][0])
 
             if( num > numeros[n][0]){
                 numero_mas_bajo = false
-
             }
-
         }
 
         // Si es el numero mas bajo lo barramos del array...
         // Pintamos encima del numero para taparlo....
         // En caso contario informamos...
-        alert(numero_mas_bajo)
+
+        //alert(numero_mas_bajo)
+        
         if(numero_mas_bajo==true){
 
             // Pintamos ese espacio...
-             // Borramos el numero del array...
+            // Borramos el numero del array...
+            
             for(n = 0; n < 100; n++){
                 if(num == numeros[n][0]){
 
-                    ctx.fillStyle = "white"
+                    ctx.fillStyle = "#F8F8FF"
                     ctx.fillRect(numeros[n][2], numeros[n][1], 50, 50)
                     numeros[n][0] = 1000000;
-
                 }
             }
-
-
 
         }else{ 
             alert(num + ": no es el numeor mas bajo, sigue intentandolo...")
